c5b6b8a801106940cefc97d0857d1670c2ebea38
FrancoNamay <166260325+FrancoNamay@users.noreply.github.com>
2024-08-26 17:31:38 -0500

    update 4

--- index.php
+++ index.php
@@ -86,7 +86,7 @@
     </nav>
 
     <div class="container px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
-        <h1 class="display-4 text-info">Registrando datos with Railway</h1>
+        <h1 class="display-4 text-info">Registrando datos with Render</h1>
         <p class="lead">PostgreSQL + PHP</p>
     </div>
 
